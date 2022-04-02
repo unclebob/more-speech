e726c21f06d10716734bbb83b4a19d34f09332ee
Robert C. Martin <unclebob@cleancoder.com>
2022-04-02 12:21:33 -0500

    Can add multiple lines to edit window.  Can navigate with arrows.  Ignores control keys, etc.

--- spec/more_speech/ui/edit_window_spec.clj
+++ spec/more_speech/ui/edit_window_spec.clj
@@ -9,11 +9,51 @@
     (it "creates text field if none exists"
       (let [frame {}
             frame (add-char frame \a)]
-        (should= "a" (:text frame))))
+        (should= ["a"] (:text frame))
+        (should= [1 0] (:insertion-point frame))))
 
     (it "appends to text field if exists."
-      (let [frame {:text "a"}
-                  frame (add-char frame \a)]
-              (should= "aa" (:text frame))))
+      (let [frame {:text ["a"]
+                   :insertion-point [1 0]}
+            frame (add-char frame \a)]
+        (should= ["aa"] (:text frame))
+        (should= [2 0] (:insertion-point frame))))
 
-      ))
+    (it "inserts into first line of text field"
+      (let [frame {:text ["_____"]
+                   :insertion-point [3 0]}
+            frame (add-char frame \i)]
+        (should= ["___i__"] (:text frame))
+        (should= [4 0] (:insertion-point frame)))
+      )
+
+    (it "inserts into second line of text field"
+      (let [frame {:text ["_____" "_____"]
+                   :insertion-point [3 1]}
+            frame (add-char frame \i)]
+        (should= ["_____" "___i__"] (:text frame))
+        (should= [4 1] (:insertion-point frame)))
+      )
+
+    (it "appends new lines"
+      (let [frame {:text ["a"]
+                   :insertion-point [1 0]}
+            frame (add-char frame \newline)]
+        (should= ["a" ""] (:text frame))
+        (should= [0 1] (:insertion-point frame))))
+
+    (it "prepends new lines"
+          (let [frame {:text ["a"]
+                       :insertion-point [0 0]}
+                frame (add-char frame \newline)]
+            (should= ["" "a"] (:text frame))
+            (should= [0 1] (:insertion-point frame))))
+
+    (it "inserts new lines"
+          (let [frame {:text ["a" "_____" "b"]
+                       :insertion-point [3 1]}
+                frame (add-char frame \newline)]
+            (should= ["a" "___" "__" "b"] (:text frame))
+            (should= [0 2] (:insertion-point frame))))
+    )
+  )
--- src/more_speech/ui/edit_window.clj
+++ src/more_speech/ui/edit_window.clj
@@ -25,23 +25,78 @@
         g (:graphics application)
         _ (g/text-align g [:left])
         _ (g/text-color g config/black)
-        cursor (cursor/->cursor g 0 (g/line-height g) 20)
-        text (get frame :text "")
-        cursor (cursor/draw-text cursor text)]
-    (cursor/draw cursor))
+        [x y] (get frame :insertion-point [0 0])]
+    (loop [cursor (cursor/->cursor g 0 (g/line-height g) 20)
+           lines (get frame :text [""])
+           line-num 0]
+      (if (empty? lines)
+        nil
+        (let [line (first lines)
+              insertion-cursor (cursor/set-pos cursor x)
+              cursor (cursor/draw-text cursor line)
+              cursor (cursor/new-lines cursor 1)
+              cursor (cursor/set-x cursor 0)]
+          (when (= line-num y)
+            (cursor/draw insertion-cursor))
+          (recur cursor (rest lines) (inc line-num))))))
   state
   )
 
-(declare add-char)
-(defn edit-window-key-pressed [state frame key]
-  (let [char (:raw-key key)
-        frame (add-char frame char)
-        frame-path (:path frame)]
-    (assoc-in state frame-path frame))
+(declare add-char move-insertion)
+(defn edit-window-key-pressed [state frame {:keys [key raw-key] :as key-struct}]
+  (prn key-struct)
+  (condp = key
+    :shift state
+    :alt state
+    :command state
+    :unknown-key state
+    :control state
+
+    :right (move-insertion state frame [1 0])
+    :left (move-insertion state frame [-1 0])
+    :up (move-insertion state frame [0 -1])
+    :down (move-insertion state frame [0 1])
+
+    (let [frame (add-char frame raw-key)
+          frame-path (:path frame)]
+      (assoc-in state frame-path frame)))
   )
 
+(defn move-insertion [state frame [x y]]
+  (let [[ix iy] (get frame :insertion-point [0 0])
+        text (get frame :text [""])
+        line (nth text iy)
+        max-x (count line)
+        max-y (dec (count text))
+        ix (+ ix x)
+        iy (+ iy y)
+        ix (max 0 ix)
+        ix (min max-x ix)
+        iy (max 0 iy)
+        iy (min max-y iy)
+        frame (assoc frame :insertion-point [ix iy])
+        frame-path (:path frame)
+        ]
+    (assoc-in state frame-path frame)))
+
 (defn add-char [frame char]
-  (let [text (get frame :text "")
-        text (str text char)
-        frame (assoc frame :text text)]
-    frame))
+  (let [text (get frame :text [""])
+        [x y] (get frame :insertion-point [0 0])
+        line (nth text y)]
+    (cond
+      (= char \newline)
+      (let [line1 (subs line 0 x)
+            line2 (subs line x)
+            text1 (take y text)
+            text2 (drop (inc y) text)
+            text (concat text1 [line1 line2] text2)
+            frame (assoc frame :text (vec text)
+                               :insertion-point [0 (inc y)])]
+        frame)
+
+      :else
+      (let [line (str (subs line 0 x) char (subs line x))
+            text (assoc text y line)
+            frame (assoc frame :text (vec text)
+                               :insertion-point [(inc x) y])]
+        frame))))
