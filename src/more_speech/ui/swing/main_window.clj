3ef3077c8723bbf1eb37051d3699af3ac9c6442e
Robert C. Martin <unclebob@cleancoder.com>
2022-05-17 16:50:58 -0500

    Set up relay control panel with R/W checkboxes.

--- src/more_speech/ui/swing/main_window.clj
+++ src/more_speech/ui/swing/main_window.clj
@@ -51,8 +51,19 @@
     (show! main-frame)
     (.start timer)))
 
+(defn make-relay-control-panel [relay]
+  (let [read-check-box (checkbox :text "R")
+        write-check-box (checkbox :text "W")
+        relay-label (flow-panel :align :left
+                                :items [(label :text relay :halign :left)])
+        buttons (flow-panel :align :left
+                            :items [read-check-box write-check-box])
+        control-panel (vertical-panel :items [relay-label buttons (separator :border 1)])]
+    control-panel))
+
 (defn make-relay-panel []
-  (let [relay-panel (listbox :model protocol/relays)]
+  (let [relay-control-panels (for [relay protocol/relays] (make-relay-control-panel relay))
+        relay-panel (vertical-panel :items relay-control-panels)]
     relay-panel))
 
 (defn timer-action [_]
