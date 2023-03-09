6651679d9ed67b35617f0a2d9c8dbc95222c42ef
Robert C. Martin <unclebob@cleancoder.com>
2023-03-09 08:21:49 -0600

    We don't need to right click on relay read and write to bring up the menu.

--- src/more_speech/ui/swing/relay_manager.clj
+++ src/more_speech/ui/swing/relay_manager.clj
@@ -229,23 +229,21 @@
     (config! relays-menu :enabled? true)))
 
 (defn read-click [url e]
-  (when (.isPopupTrigger e)
-    (let [x (.x (.getPoint e))
-          y (.y (.getPoint e))
-          label (.getComponent e)
-          p (popup :items [(action :name "none" :handler (partial set-relay-read label url :read-none))
-                           (action :name "all" :handler (partial set-relay-read label url :read-all))
-                           (action :name "trusted" :handler (partial set-relay-read label url :read-trusted))
-                           (action :name "web of trust" :handler (partial set-relay-read label url :read-web-of-trust))])]
-      (.show p (to-widget e) x y))))
+  (let [x (.x (.getPoint e))
+        y (.y (.getPoint e))
+        label (.getComponent e)
+        p (popup :items [(action :name "none" :handler (partial set-relay-read label url :read-none))
+                         (action :name "all" :handler (partial set-relay-read label url :read-all))
+                         (action :name "trusted" :handler (partial set-relay-read label url :read-trusted))
+                         (action :name "web of trust" :handler (partial set-relay-read label url :read-web-of-trust))])]
+    (.show p (to-widget e) x y)))
 
 (defn write-click [url e]
-  (when (.isPopupTrigger e)
-    (let [x (.x (.getPoint e))
-          y (.y (.getPoint e))
-          label (.getComponent e)
-          p (popup :items [(action :name "true" :handler (partial set-relay-write label url true))
-                           (action :name "false" :handler (partial set-relay-write label url false))
-                           ])]
-      (.show p (to-widget e) x y))))
+  (let [x (.x (.getPoint e))
+        y (.y (.getPoint e))
+        label (.getComponent e)
+        p (popup :items [(action :name "true" :handler (partial set-relay-write label url true))
+                         (action :name "false" :handler (partial set-relay-write label url false))
+                         ])]
+    (.show p (to-widget e) x y)))
 
