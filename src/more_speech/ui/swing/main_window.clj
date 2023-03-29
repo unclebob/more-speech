f6359e29b20854112ce906cb57733d20289fee6b
Robert C. Martin <unclebob@cleancoder.com>
2023-03-29 16:13:03 -0500

    Move stats window into stats-window.clj

--- src/more_speech/ui/swing/main_window.clj
+++ src/more_speech/ui/swing/main_window.clj
@@ -7,6 +7,7 @@
             [more-speech.ui.swing.article-panel :as article-panel]
             [more-speech.ui.swing.tabs :as tabs]
             [more-speech.ui.swing.relay-manager :as relay-manager]
+            [more-speech.ui.swing.stats-window :as stats-window]
             [more-speech.mem :refer :all]
             [more-speech.config :as config :refer [get-db]]
             [more-speech.ui.formatter-util :as formatter-util]
@@ -28,74 +29,11 @@
         name (formatter-util/abbreviate (:name profile) 20)]
     (format "%-20s %s %s" name (util/num32->hex-string id) (:picture profile))))
 
-(defn show-kinds [stats-panel]
-  (doseq [kind (keys (get-mem [:event-counter :kinds]))]
-    (let [id (keyword (str "#kind-" kind))]
-      (config! (select stats-panel [id])
-               :text (str (get-mem [:event-counter :kinds kind]))))))
-
-(defn show-status [stats-panel]
-  (config! (select stats-panel [:#backlog-data])
-           :text (str (get-mem :websocket-backlog)))
-  (config! (select stats-panel [:#processed-data])
-           :text (str (get-mem [:event-counter :total])))
-  (config! (select stats-panel [:#incoming-data])
-           :text (str (get-mem [:incoming-events])))
-  (config! (select stats-panel [:#dups-data])
-           :text (str (get-mem [:event-counter :dups])))
-  (show-kinds stats-panel))
-
-(defn close-stats-frame [timer menu _e]
-  (config! menu :enabled? true)
-  (.cancel timer))
-
-(defn make-stat-panel [name id]
-  (let [stat-label (label name)
-        stat-data (label :text "" :id id :size [100 :by 20])
-        stat-panel (left-right-split stat-data stat-label)]
-    stat-panel))
-
-(defn make-kind-panels []
-  (loop [kinds (sort (keys (get-mem [:event-counter :kinds])))
-         kind-panels []]
-    (if (empty? kinds)
-      kind-panels
-      (let [kind (first kinds)
-            kind-panel (make-stat-panel (str "Kind:" kind)
-                                        (keyword (str "kind-" kind)))]
-        (recur (rest kinds) (conj kind-panels kind-panel)))))
-  )
-
-(defn make-stats-frame [_e]
-  (let [stats-frame (frame :title "Stats")
-        incoming-panel (make-stat-panel "Incoming events." :incoming-data)
-        backlog-panel (make-stat-panel "Backlog." :backlog-data)
-        processed-panel (make-stat-panel "Processed events." :processed-data)
-        dups-panel (make-stat-panel "Duplicate events." :dups-data)
-        kind-panels (make-kind-panels)
-
-        stats-panel (vertical-panel :items (concat [incoming-panel
-                                                    processed-panel
-                                                    backlog-panel
-                                                    dups-panel]
-                                                   kind-panels))
-
-        stats-timer (Timer. "stats timer")
-        show-status-task (proxy [TimerTask] []
-                           (run [] (show-status stats-panel)))
-
-        stats-menu (select (get-mem :frame) [:#stats-menu])]
-    (config! stats-frame :content stats-panel)
-    (config! stats-menu :enabled? false)
-    (listen stats-frame :window-closing (partial close-stats-frame stats-timer stats-menu))
-    (pack! stats-frame)
-    (show! stats-frame)
-    (.schedule stats-timer show-status-task 1000 1000)))
-
 (defn make-menubar []
   (let [relays-item (menu-item :action (action :name "Relays..." :handler relay-manager/show-relay-manager)
                                :id :relays-menu)
-        stats-item (menu-item :action (action :name "Stats..." :handler make-stats-frame)
+        stats-item (menu-item :action (action :name "Stats..."
+                                              :handler stats-window/make-stats-frame)
                               :id :stats-menu)
         users-item (menu-item :text "Users...")
         profile-item (menu-item :text "Profile...")
---
+++ src/more_speech/ui/swing/stats_window.clj
@@ -0,0 +1,69 @@
+(ns more-speech.ui.swing.stats-window
+  (:require [more-speech.mem :refer :all])
+   (:use [seesaw core])
+   (:import (java.util Timer TimerTask)))
+
+
+(defn show-kinds [stats-panel]
+  (doseq [kind (keys (get-mem [:event-counter :kinds]))]
+    (let [id (keyword (str "#kind-" kind))]
+      (config! (select stats-panel [id])
+               :text (str (get-mem [:event-counter :kinds kind]))))))
+
+(defn show-status [stats-panel]
+  (config! (select stats-panel [:#backlog-data])
+           :text (str (get-mem :websocket-backlog)))
+  (config! (select stats-panel [:#processed-data])
+           :text (str (get-mem [:event-counter :total])))
+  (config! (select stats-panel [:#incoming-data])
+           :text (str (get-mem [:incoming-events])))
+  (config! (select stats-panel [:#dups-data])
+           :text (str (get-mem [:event-counter :dups])))
+  (show-kinds stats-panel))
+
+(defn close-stats-frame [timer menu _e]
+  (config! menu :enabled? true)
+  (.cancel timer))
+
+(defn make-stat-panel [name id]
+  (let [stat-label (label name)
+        stat-data (label :text "" :id id :size [100 :by 20])
+        stat-panel (left-right-split stat-data stat-label)]
+    stat-panel))
+
+(defn make-kind-panels []
+  (loop [kinds (sort (keys (get-mem [:event-counter :kinds])))
+         kind-panels []]
+    (if (empty? kinds)
+      kind-panels
+      (let [kind (first kinds)
+            kind-panel (make-stat-panel (str "Kind:" kind)
+                                        (keyword (str "kind-" kind)))]
+        (recur (rest kinds) (conj kind-panels kind-panel)))))
+  )
+
+(defn make-stats-frame [_e]
+  (let [stats-frame (frame :title "Stats")
+        incoming-panel (make-stat-panel "Incoming events." :incoming-data)
+        backlog-panel (make-stat-panel "Backlog." :backlog-data)
+        processed-panel (make-stat-panel "Processed events." :processed-data)
+        dups-panel (make-stat-panel "Duplicate events." :dups-data)
+        kind-panels (make-kind-panels)
+
+        stats-panel (vertical-panel :items (concat [incoming-panel
+                                                    processed-panel
+                                                    backlog-panel
+                                                    dups-panel]
+                                                   kind-panels))
+
+        stats-timer (Timer. "stats timer")
+        show-status-task (proxy [TimerTask] []
+                           (run [] (show-status stats-panel)))
+
+        stats-menu (select (get-mem :frame) [:#stats-menu])]
+    (config! stats-frame :content stats-panel)
+    (config! stats-menu :enabled? false)
+    (listen stats-frame :window-closing (partial close-stats-frame stats-timer stats-menu))
+    (pack! stats-frame)
+    (show! stats-frame)
+    (.schedule stats-timer show-status-task 1000 1000)))
