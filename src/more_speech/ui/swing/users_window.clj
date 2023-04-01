dc3cc2e670be1141106eb9a2162fe470e0c7458b
Robert C. Martin <unclebob@cleancoder.com>
2023-04-01 14:23:16 -0500

    Pull the web of trust into the selected list of the Users window.

--- src/more_speech/ui/swing/users_window.clj
+++ src/more_speech/ui/swing/users_window.clj
@@ -10,8 +10,7 @@
 (defn make-users-frame [_e]
   (let [users-menu (select (get-mem :frame) [:#users-menu])
         users-frame (frame :title "Users")
-        trusted-users-listbox (listbox :id :trusted-users-listbox
-                                       :size [500 :by 800])
+        trusted-users-listbox (listbox :id :trusted-users-listbox)
         trusted-users (contact-list/get-trustees)
         trusted-user-names (sort (map #(formatters/format-user-id % 50 20) trusted-users))
         trusted-users-panel (vertical-panel
@@ -23,7 +22,9 @@
         search-checkbox (checkbox :text "Search")
         search-field (text :text "" :editable? true)
         search-panel (horizontal-panel :items [search-checkbox search-field])
-        selected-listbox (listbox :size [500 :by 800])
+        selected-listbox (listbox)
+        web-of-trust-users (contact-list/get-web-of-trust)
+        web-of-trust-names (sort (map #(formatters/format-user-id % 50 20) web-of-trust-users))
         scrollable-selected-listbox (scrollable selected-listbox
                                                 :size [500 :by 800])
         selection-panel (vertical-panel :items [web-of-trust-checkbox
@@ -34,6 +35,7 @@
 
         users-panel (horizontal-panel :items [trusted-users-panel trust-button selection-panel])]
     (config! trusted-users-listbox :model trusted-user-names)
+    (config! selected-listbox :model web-of-trust-names)
     (config! users-frame :content users-panel)
     (listen users-frame :window-closing (partial close-users-frame users-menu))
     (config! users-menu :enabled? false)
