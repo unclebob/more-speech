3f180ca2b0d3c5f700b8fcacdebce9b90306431a
Robert C. Martin <unclebob@cleancoder.com>
2023-04-04 17:19:45 -0500

    Get ready for trusting selected users.

--- src/more_speech/ui/swing/users_window.clj
+++ src/more_speech/ui/swing/users_window.clj
@@ -1,8 +1,10 @@
 (ns more-speech.ui.swing.users-window
   (:require
+    [clojure.set :as set]
     [more-speech.config :as config]
     [more-speech.config :refer [get-db]]
     [more-speech.db.gateway :as gateway]
+    [more-speech.mem :refer :all]
     [more-speech.mem :refer :all]
     [more-speech.nostr.contact-list :as contact-list]
     [more-speech.nostr.util :as util]
@@ -10,7 +12,8 @@
   (:use (seesaw [core])))
 
 (defn close-users-frame [users-menu _e]
-  (config! users-menu :enabled? true))
+  (config! users-menu :enabled? true)
+  (set-mem :user-window nil))
 
 (defn make-sorted-listbox-items [ids]
   (sort-by first
@@ -18,62 +21,89 @@
                 ids))
   )
 
-(defn load-recent-authors [frame _e]
-  (let [after (- (util/get-now) (* 86400 1))
-        recent-authors (gateway/get-some-recent-event-authors (get-db) after)
-        recent-author-items (make-sorted-listbox-items recent-authors)
-        selected-listbox (select frame [:#selected-users])]
-    (config! selected-listbox :model recent-author-items)))
-
-(defn load-web-of-trust-authors [frame authors _e]
-  (config! (select frame [:#selected-users]) :model authors))
-
 (defn render-listbox-item [widget item]
   (let [value (:value item)]
     (if (string? value)
       (text! widget value)
       (text! widget (first value)))))
 
-(defn make-users-frame [_e]
-  (let [users-menu (select (get-mem :frame) [:#users-menu])
-        users-frame (frame :title "Users")
-        trusted-users-listbox (listbox :id :trusted-users-listbox
-                                       :font config/default-font
-                                       :renderer render-listbox-item)
-        trusted-users (contact-list/get-trustees)
-        trusted-user-items (make-sorted-listbox-items trusted-users)
-        web-of-trust-users (contact-list/get-web-of-trust)
-        web-of-trust-items (make-sorted-listbox-items web-of-trust-users)
-        trusted-users-panel (vertical-panel
-                              :items [(label "Trusted")
-                                      (scrollable trusted-users-listbox :size [500 :by 800])])
-        trust-button (button :text "<-")
-        selection-group (button-group)
-        web-of-trust-button (radio :text "Web of trust"
-                                   :group selection-group
-                                   :selected? true
-                                   :listen [:action (partial load-web-of-trust-authors users-frame web-of-trust-items)])
-        recent-button (radio :text "Recent users"
-                             :group selection-group
-                             :listen [:action (partial load-recent-authors users-frame)])
+(defn trust-selection [frame _e]
+  (let [selected-listbox (select frame [:#selected-users])
+        selected-item (selection selected-listbox)]
+    (when (some? selected-item)
+      )))
 
-        search-field (text :text "" :editable? true)
-        selected-listbox (listbox :id :selected-users
-                                  :font config/default-font
-                                  :renderer render-listbox-item)
-        scrollable-selected-listbox (scrollable selected-listbox
-                                                :size [500 :by 800])
-        selection-panel (vertical-panel :items [web-of-trust-button
-                                                recent-button
-                                                search-field
-                                                scrollable-selected-listbox]
-                                        )
+(defn load-web-of-trust-authors [frame  _e]
+  (config! (select frame [:#selected-users])
+           :model (get-mem [:user-window :web-of-trust-items])))
 
-        users-panel (horizontal-panel :items [trusted-users-panel trust-button selection-panel])]
-    (config! trusted-users-listbox :model trusted-user-items)
-    (config! selected-listbox :model web-of-trust-items)
-    (config! users-frame :content users-panel)
-    (listen users-frame :window-closing (partial close-users-frame users-menu))
-    (config! users-menu :enabled? false)
-    (pack! users-frame)
-    (show! users-frame)))
+(defn load-recent-users [frame _e]
+  (let [after (- (util/get-now) (* 86400 1))
+        recent-users (set/difference
+                         (set (gateway/get-some-recent-event-authors (get-db) after))
+                         (set (get-mem [:user-window :trusted-users])))
+        recent-user-items (make-sorted-listbox-items recent-users)
+        selected-listbox (select frame [:#selected-users])]
+    (set-mem [:user-window :recent-users] recent-users)
+    (set-mem [:user-window :recent-user-items] recent-user-items)
+    (config! selected-listbox :model recent-user-items)))
+
+(defn load-trusted-users []
+  (let [trusted-users (contact-list/get-trustees)]
+    (set-mem [:user-window :trusted-users] trusted-users)
+    (set-mem [:user-window :trusted-user-items]
+             (make-sorted-listbox-items trusted-users))))
+
+(defn load-web-of-trust-users []
+  (let [web-of-trust-users (set/difference
+                             (set (contact-list/get-web-of-trust))
+                             (set (get-mem [:user-window :trusted-users])))]
+    (set-mem [:user-window :web-of-trust-users] web-of-trust-users)
+    (set-mem [:user-window :web-of-trust-items]
+             (make-sorted-listbox-items web-of-trust-users))))
+
+  (defn load-user-window-data []
+    (load-trusted-users)
+    (load-web-of-trust-users))
+
+  (defn make-users-frame [_e]
+    (let [users-menu (select (get-mem :frame) [:#users-menu])
+          users-frame (frame :title "Users")
+          trusted-users-listbox (listbox :id :trusted-users-listbox
+                                         :font config/default-font
+                                         :renderer render-listbox-item)
+          trusted-users-panel (vertical-panel
+                                :items [(label "Trusted")
+                                        (scrollable trusted-users-listbox :size [500 :by 800])])
+          trust-button (button :text "<-"
+                               :listen [:action (partial trust-selection users-frame)])
+          selection-group (button-group)
+          web-of-trust-button (radio :text "Web of trust"
+                                     :group selection-group
+                                     :selected? true
+                                     :listen [:action (partial load-web-of-trust-authors users-frame)])
+          recent-button (radio :text "Recent users"
+                               :group selection-group
+                               :listen [:action (partial load-recent-users users-frame)])
+
+          search-field (text :text "" :editable? true)
+          selected-listbox (listbox :id :selected-users
+                                    :font config/default-font
+                                    :renderer render-listbox-item)
+          scrollable-selected-listbox (scrollable selected-listbox
+                                                  :size [500 :by 800])
+          selection-panel (vertical-panel :items [web-of-trust-button
+                                                  recent-button
+                                                  search-field
+                                                  scrollable-selected-listbox])
+
+
+          users-panel (horizontal-panel :items [trusted-users-panel trust-button selection-panel])]
+      (load-user-window-data)
+      (config! trusted-users-listbox :model (get-mem [:user-window :trusted-user-items]))
+      (config! selected-listbox :model (get-mem [:user-window :web-of-trust-items]))
+      (config! users-frame :content users-panel)
+      (listen users-frame :window-closing (partial close-users-frame users-menu))
+      (config! users-menu :enabled? false)
+      (pack! users-frame)
+      (show! users-frame)))
