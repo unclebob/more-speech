46b91257a41664422e5783c1e97ec2e41fb45407
Robert C. Martin <unclebob@cleancoder.com>
2023-05-12 07:34:53 -0500

    Update component versions and cleanup project.clj.

--- project.clj
+++ project.clj
@@ -5,25 +5,22 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/data.json "2.4.0"]
-                 [org.clojure/core.async "1.5.648"]
-                 [clojure.java-time "0.3.3"]
+                 [org.clojure/core.async "1.6.673"]
+                 [clojure.java-time "1.2.0"]
                  [javax.xml.bind/jaxb-api "2.4.0-b180830.0359"]
                  [seesaw "1.5.0"]
                  [org.bouncycastle/bcprov-jdk18on "1.72"]
-                 [com.xtdb/xtdb-core "1.23.0"]
-                 [com.xtdb/xtdb-rocksdb "1.23.0"]
-                 [org.slf4j/slf4j-nop "1.7.30"]
+                 [com.xtdb/xtdb-core "1.23.1"]
+                 [com.xtdb/xtdb-rocksdb "1.23.1"]
+                 [org.slf4j/slf4j-nop "1.7.30"] ;1.7.30 works.
                  [clj-http "3.12.3"]
                  [org.clojure/test.check "1.1.1"]]
-  :profiles {:dev {:dependencies [[speclj "3.4.1"]]}
+  :profiles {:dev {:dependencies [[speclj "3.4.3"]]}
              :uberjar {:aot :all}}
   :repositories {"local" {:url "file:lib" :username "" :password ""}}
-  :plugins [[speclj "3.4.1"]]
+  :plugins [[speclj "3.4.3"]]
   :test-paths ["spec"]
   :main more-speech.core
-  ;:java-cmd "/Users/unclebob/Library/Java/JavaVirtualMachines/openjdk-17.0.2/Contents/Home/bin/java"
   :java-source-paths ["java"]
-;  :resource-paths ["jars/bcprov-jdk18on-171.jar"]
-  ; remember to deploy: lein deploy local bcprov 1.7.1 jars/bcprov-jdk18on-171.jar
 )
 
