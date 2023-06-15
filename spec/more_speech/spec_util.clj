0e45afcc64c100274b24dc29f4e203ef314bd003
Robert C. Martin <unclebob@cleancoder.com>
2023-06-15 05:16:29 -0500

    clean up the macro a bit. Can't figure out a better solution for the 'list'

--- spec/more_speech/spec_util.clj
+++ spec/more_speech/spec_util.clj
@@ -1,7 +1,15 @@
-(ns more-speech.spec-util)
+(ns more-speech.spec-util
+  (:require
+    [more-speech.config :as config]
+    [more-speech.db.in-memory :as in-memory]
+    [more-speech.mem :as mem]
+    [speclj.core :refer :all]))
 
 (defmacro setup-db-mem []
-  '(list (with db (more-speech.db.in-memory/get-db))
-         (before-all (more-speech.config/set-db! :in-memory))
-         (before (more-speech.db.in-memory/clear-db @db)
-                 (more-speech.mem/clear-mem))))
+  `(list (with ~'db (in-memory/get-db))
+         (before-all (config/set-db! :in-memory))
+         (before (in-memory/clear-db @~'db)
+                 (mem/clear-mem))))
+
+;the list forces the execution of the three statements.
+;(prn (macroexpand '(setup-db-mem)))
